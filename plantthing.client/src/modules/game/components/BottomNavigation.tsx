a268082760e279b214803d8bff60903124c1c752
Joseph Henry Passineau <joseph.passineau@ariv.ca>
2025-03-23 08:36:09 -0400

    Force text-center on bottomNavigation buttons

--- plantthing.client/src/modules/game/components/BottomNavigation.tsx
+++ plantthing.client/src/modules/game/components/BottomNavigation.tsx
@@ -18,22 +18,22 @@ export const BottomNavigation = () => {
 
   return (
     <div className="flex absolute bottom-0 p-2 left-0 w-full justify-center">
-      <div className="flex gap-2 justify-around">
+      <div className="flex gap-2 justify-around text-center">
         <Button
           onClick={handleShopOnClick}
-          className="flex flex-col text-sm w-20 bg-green-500"
+          className="flex flex-col text-sm w-20 bg-green-500 text-center"
         >
           <i className="text-3xl hn hn-shopping-cart-solid"></i>Shop
         </Button>
         <Button
           onClick={handleQuestOnClick}
-          className="flex flex-col text-sm w-20 bg-blue-500"
+          className="flex flex-col text-sm w-20 bg-blue-500 text-center"
         >
           <i className="text-3xl hn hn-crown-solid"></i>Quests
         </Button>
         <Button
           onClick={handleSettingsOnClick}
-          className="flex flex-col text-sm w-20 bg-orange-500"
+          className="flex flex-col text-sm w-20 bg-orange-500 text-center"
         >
           <i className="text-3xl hn hn-cog-solid"></i>Settings
         </Button>
